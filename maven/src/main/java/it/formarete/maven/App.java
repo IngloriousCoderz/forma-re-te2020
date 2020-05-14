d2546a032df9142b40300a24e82a9328c8023b41
IceOnFire <antony.mistretta@gmail.com>
2020-05-14 18:40:53 +0200

    Add Maven project

---
+++ maven/src/main/java/it/formarete/maven/App.java
@@ -0,0 +1,13 @@
+package it.formarete.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven/src/test/java/it/formarete/maven/AppTest.java
@@ -0,0 +1,13 @@
+package it.formarete.maven;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertThat(true, is(true));
+	}
+}
